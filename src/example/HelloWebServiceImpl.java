226853a66852c28f48599e00ef49a36ee221c0ba
spartacusxxxxxx <spartacusxxxxxx@gmail.com>
2016-08-24 12:36:43 +0300

    The WebService is done complete

--- src/example/HelloWebServiceImpl.java
+++ src/example/HelloWebServiceImpl.java
@@ -5,6 +5,8 @@ package example;
 import javax.jws.WebService;
 
 import java.io.*;
+import java.io.IOException;
+
 
 // но здесь используется с параметром endpointInterface,
 // указывающим полное имя класса интерфейса нашего веб-сервиса
@@ -29,20 +31,37 @@ public class HelloWebServiceImpl implements HelloWebServiceIntf
     {
         String tempStr = Integer.toString(number);
 
-        try(FileOutputStream fosJournal = new FileOutputStream("$HOME/TempTestDir/Journal.txt"))
-        {
 
+        //FileOutputStream fosJournal = null;
+        FileWriter writer = null;
+        try
+        {
+            //fosJournal = new FileOutputStream("/Users/mihail/TempTestDir/Journal.rtf");
+            writer = new FileWriter("/Users/mihail/TempTestDir/Journal.txt", true);
 
             // перевод строки в байты
-            byte[] buffer = tempStr.getBytes();
+            char[] buffer = tempStr.toCharArray();
+           // byte[] buffer = tempStr.getBytes();
 
-            fosJournal.write(buffer, 0, buffer.length);
+            //fosJournal.write(buffer, 0, buffer.length);
+            writer.write(buffer, 0, buffer.length);
+            writer.append("   UnsignedInteger value");
+            writer.append('\n');
+            writer.flush();
         }
         catch(IOException ex)
         {
             //throw ex.getCause();
             System.out.println(ex.getMessage());
         }
+        finally
+        {
+            if (writer != null)
+            {
+               // writer.close();
+            }
+        }
+
 
         return "OK " + tempStr;
     }
@@ -57,19 +76,35 @@ public class HelloWebServiceImpl implements HelloWebServiceIntf
     {
         String tempStr = Integer.toString(number);
 
-        try(FileOutputStream fosJournal = new FileOutputStream("$HOME/TempTestDir/Journal.txt"))
-        {
 
+        FileWriter writer = null;
+        //FileOutputStream fosJournal = null;
+        try
+        {
+            //fosJournal = new FileOutputStream("/Users/mihail/TempTestDir/Journal.rtf");
+            writer = new FileWriter("/Users/mihail/TempTestDir/Journal.txt", true);
 
             // перевод строки в байты
-            byte[] buffer = tempStr.getBytes();
+            char[] buffer = tempStr.toCharArray();
+           // byte[] buffer = tempStr.getBytes();
 
-            fosJournal.write(buffer, 0, buffer.length);
+            //fosJournal.write(buffer, 0, buffer.length);
+            writer.write(buffer, 0, buffer.length);
+            writer.append("   SignedInteger value");
+            writer.append('\n');
+            writer.flush();
         }
         catch(IOException ex)
         {
             //throw ex.getCause();
             System.out.println(ex.getMessage());
+        }
+        finally
+        {
+            if (writer != null)
+            {
+               // writer.close();
+            }
         }
 
         return "OK " + tempStr;
@@ -79,24 +114,45 @@ public class HelloWebServiceImpl implements HelloWebServiceIntf
 
 
     @Override
-    //целое отрицательное число
+    //число с плавающей точкой
     public String putFloat(float number)
     {
         String tempStr = Float.toString(number);
 
-        try(FileOutputStream fosJournal = new FileOutputStream("$HOME/TempTestDir/Journal.txt"))
-        {
 
+        //FileOutputStream fosJournal = null;
+        FileWriter writer = null;
+
+        try
+        {
+            //fosJournal = new FileOutputStream("/Users/mihail/TempTestDir/Journal.txt");
+            writer = new FileWriter("/Users/mihail/TempTestDir/Journal.txt", true);
 
             // перевод строки в байты
-            byte[] buffer = tempStr.getBytes();
+            char[] buffer = tempStr.toCharArray();
+            //byte[] buffer = tempStr.getBytes();
 
-            fosJournal.write(buffer, 0, buffer.length);
+            //fosJournal.write(buffer, 0, buffer.length);
+            //fosJournal.close();
+            //fosJournal.
+            writer.write(buffer, 0, buffer.length);
+            writer.append("   float value");
+            writer.append('\n');
+            writer.flush();
+
+
         }
         catch(IOException ex)
         {
             //throw ex.getCause();
             System.out.println(ex.getMessage());
+        }
+        finally
+        {
+            if (writer != null)
+            {
+                //writer.close();
+            }
         }
 
         return "OK " + tempStr;
@@ -108,21 +164,76 @@ public class HelloWebServiceImpl implements HelloWebServiceIntf
     public String putFile(String FileName)
     {
 
-        try(FileInputStream fin = new FileInputStream(FileName);
-            FileOutputStream fos = new FileOutputStream("$HOME/TempTestDir/Res.bin"))
+        long currentTime = System.currentTimeMillis();
+        String curTime = Long.toString(currentTime);
+        String FileNameExpansion = null;
+        String FileNameCopy = null;
+        FileWriter writer = null;
+
+        int dotIdx = FileName.lastIndexOf('.');
+        int lengthIdx = FileName.length();
+        int bslashIdx = FileName.lastIndexOf('/');
+        if (dotIdx != -1 && lengthIdx != -1 && bslashIdx != -1)
         {
+             FileNameExpansion = FileName.substring(dotIdx, lengthIdx);
+             FileNameCopy = FileName.substring(bslashIdx + 1, dotIdx);
+        }
+
+
+
+        FileInputStream fin = null;
+        FileOutputStream fos = null;
+       // PrintWriter printWriter = null;
+
+        try
+        {
+            writer = new FileWriter("/Users/mihail/TempTestDir/Journal.txt", true);
+
+            // перевод строки в байты
+            char[] buffer_ch = FileNameCopy.toCharArray();
+
+            writer.write(buffer_ch, 0, buffer_ch.length);
+            writer.append("   name of file");
+            writer.append('\n');
+            writer.flush();
+
+            fin = new FileInputStream(FileName);
+            //fos = new FileOutputStream("/Users/mihail/TempTestDir/COPY_Res.jpg", true);
+            fos = new FileOutputStream("/Users/mihail/TempTestDir/COPY_" + curTime + "_" + FileNameCopy + FileNameExpansion  , true);
+            //printWriter = new PrintWriter(new OutputStreamWriter(new FileOutputStream("/Users/mihail/TempTestDir/COPY_Res.jpg", true), "UTF-8"));
+
+            //определяем размер буфера
             byte[] buffer = new byte[fin.available()];
+
             // считываем буфер
             fin.read(buffer, 0, buffer.length);
+
             // записываем из буфера в файл
             fos.write(buffer, 0, buffer.length);
+
+
         }
         catch(IOException ex)
         {
 
             System.out.println(ex.getMessage());
         }
+        finally
+        {
+            if (fin != null)
+            {
+               // fin.close();
+            }
 
+            if (fos != null)
+            {
+                //fos.close();
+            }
+
+        }
+
+
+        return "OK ";
 
     }
 
